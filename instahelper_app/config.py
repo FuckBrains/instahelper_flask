809e1232de6ce0d09abf879dd975d5aec6e8b8dc
hakanxkaan <kaanhakan2005@gmail.com>
2020-09-12 13:35:21 +0300

    removed mongodb uri

--- instahelper_app/config.py
+++ instahelper_app/config.py
@@ -2,11 +2,6 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///insta.db"
-    MONGODB_DATABASE = "instadb"
-    MONGODB_USERNAME = "jrkha"
-    MONGODB_PASSWORD = "JrKHa425266"
-    MONGODB_PORT = ""
-    MONGODB_HOST = "mongodb+srv://jrkha:JrKHa425266@instahelper-cluster.grmna.mongodb.net/instadb?retryWrites=true&w=majority"
     SECRET_KEY = "3c25743573557e402a40537afd27"
     """MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = "587"
